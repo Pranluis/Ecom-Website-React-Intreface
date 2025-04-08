4e0a5ead5174f07f4bc9f2f45dbc59db3f41f76d
priyanshu1434 <2387745@cognizant.com>
2025-04-09 01:35:24 +0530

    added login component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import CRUD from './components/ExtraContent/CRUD'
+import Login from './components/LoginPage/login'
 
 
 function App() {
@@ -8,8 +9,8 @@ function App() {
 
   return (
     <>
-      {/* <HomePage name="Pranluis" /> */}
-      <CRUD/>
+      <Login/>
+      {/* <CRUD/> */}
       {/* <Extracomp /> */}
     </>
   )
