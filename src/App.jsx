a0094a06d7e75f6570f535f7487d35bcb584b4f1
priyanshu1434 <2387745@cognizant.com>
2025-04-09 00:56:35 +0530

    little change

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import HomePage from './components/HomePage/homePage'
 import CRUD from './components/ExtraContent/CRUD'
 
 
